97809917390e891df1d3ff0e3d370a5de72b0d3b
Thawne <2300043476@qq.com>
2023-02-02 20:22:14 +0800

    feat: add clearer example

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
-  Buttons:
-  <v-button size="mini" type="primary" />
+  <div style="display: flex; flex-direction: column">
+    <div>Buttons:</div>
+    <div class="btn-demo">
+      <span>Size </span>
+      <v-button size="mini" type="primary" />
+      <v-button size="compact" type="primary" />
+      <v-button size="default" type="primary" />
+      <v-button size="large" type="primary" />
+    </div>
+    <div class="btn-demo">
+      <span>Kind </span>
+      <v-button type="primary">Primary</v-button>
+      <v-button type="secondary">Secondary</v-button>
+      <v-button type="tertiary">Tertiary</v-button>
+    </div>
+    <div class="btn-demo">
+      <span>Shape </span>
+      <v-button shape="default">Default</v-button>
+      <v-button shape="default">Pill</v-button>
+      <v-button shape="default">Round</v-button>
+      <v-button shape="default">Circle</v-button>
+      <v-button shape="default">Square</v-button>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped>
+.btn-demo * {
+  margin-right: 10px;
+}
+</style>
