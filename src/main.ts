eea80933b28c218729c04cf8f9d85d125f2bdfcc
Thawne <2300043476@qq.com>
2023-02-17 00:36:54 +0800

    feat: update demo

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 
 import 'virtual:svg-icons-register';
-import Button from '@vuse-ui/components/button';
-import Notification from '@vuse-ui/components/notification';
-import Icon from '@vuse-ui/components/icon';
-import Textarea from '@vuse-ui/components/textarea';
-import Input from '@vuse-ui/components/input';
-import Avatar from '@vuse-ui/components/avatar';
-import Link from '@vuse-ui/components/link';
-import Breadcrumbs from '@vuse-ui/components/breadcrumbs';
+import Vuse from '../packages/components';
 
-createApp(App)
-  .use(Button)
-  .use(Notification)
-  .use(Icon)
-  .use(Textarea)
-  .use(Avatar)
-  .use(Input)
-  .use(Link)
-  .use(Breadcrumbs)
-  .mount('#app');
+createApp(App).use(Vuse).mount('#app');
