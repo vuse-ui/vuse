0ca54fbaa407b0785f23d40aa880d01695a469e9
海底烧烤店 <2041909905@qq.com>
2023-02-18 15:50:30 +0800

    docs: add list

---
+++ docs/components/list/basic.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list>
+    <v-list-item v-for="(item, index) in data" :key="index">{{ item.title }}</v-list-item>
+  </v-list>
+</template>
+<script setup lang="ts">
+interface DataItem {
+  title: string;
+}
+const data: DataItem[] = [
+  {
+    title: 'Vuse Title 1',
+  },
+  {
+    title: 'Vuse Title 2',
+  },
+  {
+    title: 'Vuse Title 3',
+  },
+  {
+    title: 'Vuse Title 4',
+  },
+];
+</script>
