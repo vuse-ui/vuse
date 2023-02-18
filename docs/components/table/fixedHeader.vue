18c16a70f20b918b9ac84c0bdd0afce1af3d2fb7
海底烧烤店 <2041909905@qq.com>
2023-02-18 22:44:20 +0800

    docs: add table fixed header

---
+++ docs/components/table/fixedHeader.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-table height="240px" bordered :dataSource="dataSource" :columns="columns"> </v-table>
+</template>
+<script setup lang="ts">
+const dataSource = [...Array(10)].map((_, i) => ({
+  key: i,
+  name: `Edward King ${i}`,
+  age: 32,
+  address: `London, Park Lane no. ${i}`,
+}));
+
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name',
+    width: 150, // 🚨 Specify width of columns
+  },
+  {
+    title: 'Age',
+    dataIndex: 'age',
+    key: 'age',
+    width: 150, // 🚨 Specify width of columns
+  },
+  {
+    title: 'Address',
+    dataIndex: 'address',
+    key: 'address',
+  },
+];
+</script>
--- docs/components/table/index.md
+++ docs/components/table/index.md
@@ -26,6 +26,12 @@ demo-preview=./HeaderAndFooter.vue
 demo-preview=./CustomCells.vue
 :::
 
+## Fixed Header
+
+:::preview Fixed Header || Display large amounts of data in scrollable view. Specify width of columns if header and cell do not align properly. If specified width is not working or have gutter between columns, please try to leave one column at least without width to fit fluid layout, or make sure no long word to break table layout.
+demo-preview=./FixedHeader.vue
+:::
+
 <style>
 .vp-doc tr {
     border-top: none;
