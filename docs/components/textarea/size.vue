fc2b20c9b334f1a088ee73a48afc08dd4f09b952
Thawne <2300043476@qq.com>
2023-02-19 20:59:18 +0800

    docs: add textarea

---
+++ docs/components/textarea/size.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-textarea size="mini" v-model="content.mini" />
+  <v-textarea size="compact" v-model="content.compact" />
+  <v-textarea size="default" v-model="content.default" />
+  <v-textarea size="large" v-model="content.large" />
+</template>
+
+<script setup lang="ts">
+const content = {
+  mini: 'Mini textarea',
+  compact: 'Compact textarea',
+  default: 'Default textarea',
+  large: 'Large textarea',
+};
+</script>
---
+++ docs/components/textarea/state.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-textarea positive placeholder="Controlled Input" v-model="content.positive" />
+  <v-textarea error placeholder="Controlled Input" v-model="content.error" />
+  <v-textarea disabled placeholder="Controlled Input" v-model="content.disabled" />
+  <v-textarea readonly placeholder="Controlled Input" v-model="content.readonly" />
+</template>
+
+<script setup lang="ts">
+const content = {
+  positive: 'Positive textarea',
+  error: 'Error textarea',
+  disabled: 'Disabled textarea',
+  readonly: 'Readonly textarea',
+};
+</script>
