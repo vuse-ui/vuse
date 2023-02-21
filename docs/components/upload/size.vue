c25cad1214591af8a8d7041831b235e95a5de498
WJL <347258143@qq.com>
2023-02-21 10:56:46 +0800

    new file:   docs/components/upload/accept.vue 	new file:   docs/components/upload/disabled.vue 	new file:   docs/components/upload/filesize.vue 	new file:   docs/components/upload/index.md 	new file:   docs/components/upload/multiple.vue 	new file:   docs/components/upload/size.vue 	new file:   docs/components/upload/vstyle.vue

---
+++ docs/components/upload/size.vue
@@ -0,0 +1,7 @@
+<template>
+  <div style="display: flex; flex-direction: column; gap: 5px">
+    <v-upload size="small"></v-upload>
+    <v-upload size="normal"></v-upload>
+    <v-upload size="large"></v-upload>
+  </div>
+</template>
---
+++ docs/components/upload/vstyle.vue
@@ -0,0 +1,6 @@
+<template>
+  <div style="display: flex; flex-direction: column; gap: 5px">
+    <v-upload vstyle="basic"></v-upload>
+    <v-upload vstyle="important"></v-upload>
+  </div>
+</template>
