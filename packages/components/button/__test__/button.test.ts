174a1e167f0bd863ec576e7f874c6b5e92af39a8
Thawne <2300043476@qq.com>
2023-02-27 17:57:42 +0800

    test: add button disabled

--- packages/components/button/__test__/button.test.ts
+++ packages/components/button/__test__/button.test.ts
@@ -1,6 +1,7 @@
 import { describe, test, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
-import Button from '../index';
+import { Button } from '../index';
+import { type, size } from '../props';
 
 describe('Button', () => {
   test('class', () => {
@@ -8,9 +9,32 @@ describe('Button', () => {
     expect(wrapper.classes()).toContain('v-button');
   });
   test('type', () => {
-    const wrapper = mount(Button, {
-      props: { type: 'secondary' },
+    type.forEach(item => {
+      const wrapper = mount(Button, {
+        props: { type: item },
+      });
+      expect(wrapper.classes()).toContain(`v-button-${item}`);
     });
-    expect(wrapper.classes()).toContain('v-button-secondary');
+  });
+  test('size', () => {
+    size.forEach(item => {
+      const wrapper = mount(Button, {
+        props: { size: item },
+      });
+      expect(wrapper.classes()).toContain(`v-button-${item}`);
+    });
+  });
+  test('loading', () => {
+    const wrapper = mount(Button, {
+      props: { loading: true },
+    });
+    expect(wrapper.classes()).toContain('v-button-loading');
+    expect(wrapper.attributes('loading')).toBe('true');
+  });
+  test('disabled', () => {
+    const wrapper = mount(Button, {
+      props: { loading: true },
+    });
+    expect(wrapper.attributes('disabled')).toBe('true');
   });
 });
